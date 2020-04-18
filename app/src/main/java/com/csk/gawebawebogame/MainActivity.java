617333db336efd7172cf18b575e4752d7b5cd9fa
cskrub33 <cskrub33@naver.com>
2020-04-18 21:14:47 +0900

    UI Second complete

--- app/src/main/java/com/csk/gawebawebogame/MainActivity.java
+++ app/src/main/java/com/csk/gawebawebogame/MainActivity.java
@@ -2,15 +2,27 @@ package com.csk.gawebawebogame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView handAnimImageView;
+
+    AnimationDrawable animationDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handAnimImageView = findViewById(R.id.hand_anim_image_view);
+        animationDrawable = (AnimationDrawable) handAnimImageView.getDrawable();
+
+        animationDrawable.start();
+
     }
 
     public void button_click(View view) {
