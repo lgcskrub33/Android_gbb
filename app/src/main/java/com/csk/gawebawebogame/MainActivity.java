290e27e4c3f00dbb8bacb5dd41ebdaac04792a88
cskrub33 <cskrub33@naver.com>
2020-04-19 22:36:42 +0900

    0419default

--- app/src/main/java/com/csk/gawebawebogame/MainActivity.java
+++ app/src/main/java/com/csk/gawebawebogame/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
     ImageView handAnimImageView;
+    ImageView setHandImageView;
+
 
     AnimationDrawable animationDrawable;
 
@@ -19,13 +21,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         handAnimImageView = findViewById(R.id.hand_anim_image_view);
+        setHandImageView = findViewById(R.id.set_hand_image_view);
+        handAnimImageView.setVisibility(View.GONE);
+        setHandImageView.setVisibility(View.VISIBLE);
+
         animationDrawable = (AnimationDrawable) handAnimImageView.getDrawable();
 
-        animationDrawable.start();
+        //animationDrawable.start();
 
     }
 
     public void button_click(View view) {
+
+        switch (view.getId()) {
+            case R.id.replay_button:
+                setHandImageView.setVisibility(View.GONE);
+                handAnimImageView.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+                break;
+
+            default:
+                animationDrawable.stop();
+                handAnimImageView.setVisibility(View.GONE);
+                setHandImageView.setVisibility(View.VISIBLE);
+                setHandImageView.setImageResource(R.drawable.com_gawe);
+
+                break;
+
+        }
+
 
     }
 
